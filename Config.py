ea0749b05999feac960a8d9de2b82f2e5dabd587
MeHetavi <hetavi.shah05@gmail.com>
2024-02-09 00:18:37 +0530

    Removed Database configuration code and added the same to another file.

--- Config.py
+++ Config.py
@@ -1,23 +1,3 @@
 from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
-
-import psycopg2
-
-# Establish connection parameters
-host = 'localhost'
-database = 'YogaConnect+'
-user = 'postgres'
-password = 'hetu'
-
-# Establish connection
-try:
-    connection = psycopg2.connect(
-        host=host,
-        database=database,
-        user=user,
-        password=password
-    )    
-    
-except psycopg2.Error as e:
-    ...
